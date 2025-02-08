05f1fcd9232237666376d08b2a41d9b237db6f6c
d <davybinde@gmail.com>
2025-02-08 22:35:32 +0100

    Ajout Cas non passant "Login - User unknow "
    Ajout cas de test non passant fichier Login.cy.js
    
    Ajout constructor userUnknow au fichier user factory
    
    Ajout 2 méthode au fichier loginPopup
    ClickWithIncorrectUserName
    CloseLoginPopup

--- cypress/e2e/testDavy/DB002_Login.cy.js
+++ cypress/e2e/testDavy/DB002_Login.cy.js
@@ -7,6 +7,9 @@ describe("Login feature", () => {
     const headerPage = new HeaderPage();
     const loginPopup = new LoginPopup();
     const user = new userFactory().newUser();
+    const userUnknown = new userFactory().userUnknown();
+
+
 
     before(() => {
             headerPage.setUp();
@@ -24,4 +27,20 @@ describe("Login feature", () => {
         .clickLoginButton()
         .clickLogoutMenu();
     });
+
+
+    it('Login avec un username incorrect', () => {  
+        headerPage.setUp();
+        headerPage.clickLoginMenu()
+        .fillUsername(userUnknown.username)
+        .fillPassword(userUnknown.password)
+        .clickLoginButtonWithIncorrectUsername()
+        .assertAlertThenAccept('User does not exist.', loginPopup)
+        .closeLoginPopup()
+        
+    });
+
+
+
+    
 });
